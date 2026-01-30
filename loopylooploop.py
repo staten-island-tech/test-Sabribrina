568746fafe531440e81ea59a63ea14923a57d843
Sabribrina <sabrinal117@nycstudents.net>
2026-01-30 09:31:47 -0500

    1/29/26

---
+++ loopylooploop.py
@@ -0,0 +1,42 @@
+""" for i in range(3):
+    print(i) """
+import turtle
+from turtle import *
+t = Turtle()
+
+""" for i in range(4):
+    t.forward(100)
+    t.left(90)
+
+for i in range(3):
+    t.forward(100)
+    t.left(120) """
+
+""" for i in range(60):
+    def square(x):
+        t.forward(x)
+        t.left(90)
+        t.forward(x)
+        t.left(90)
+        t.forward(x)
+        t.left(90)
+        t.forward(x)
+        t.left(5)
+    square(200) """
+
+""" sidelength = 100
+rotate = 90
+def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+square(100,90) """
+
+def doubleSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        doubleSquares(length, 90)
+        length += length * 2
+doubleSquares(5)
+
+turtle.done()
